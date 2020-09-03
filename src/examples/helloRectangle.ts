5e6de829e161d1a9b746381a6068be14c1cbbc97
Yan, Shaobo <shaobo.yan@intel.com>
2020-09-03 13:23:25 +0800

    WIP: rendering texture to rectangle

--- src/examples/helloRectangle.ts
+++ src/examples/helloRectangle.ts
@@ -1,157 +1,201 @@
 import glslangModule from '../glslang';
+import { createTextureFromImage } from '../helpers';
 
 export const title = 'Hello Triangle';
 export const description = 'Shows rendering a basic triangle.';
 
-function createSPIRVModule(device: GPUDevice, glslang, stage: string, glsl: string) : GPUShaderModule {
-    return device.createShaderModule({
-      code: glslang.compileGLSL(glsl, stage),
+export async function init(canvas: HTMLCanvasElement, useWGSL: boolean) {
+  const adapter = await navigator.gpu.requestAdapter();
+  const device = await adapter.requestDevice();
+  const glslang = await glslangModule();
 
-      // @ts-ignore
-      source: glsl,
-      transform: source => glslang.compileGLSL(source, stage),
-    })
-}
+  const context = canvas.getContext("gpupresent");
 
-function createWGSLModule(device: GPUDevice, wgsl: string) : GPUShaderModule {
-    return device.createShaderModule({
-      code: wgsl,
+  const swapChainFormat = "bgra8unorm";
 
-      // @ts-ignore
-      source: wgsl,
-      transform: source => source,
-    })
-}
+  const swapChain = context.configureSwapChain({
+    device,
+    format: swapChainFormat,
+  });
 
-export async function init(canvas: HTMLCanvasElement) {
-    const vertexShaderGLSL = `#version 450
-      layout(location = 0) in vec3 pos;
-      layout(location = 1) in vec2 uv;
-      
-      void main() {
-        gl_Position = vec4(pos, 1.0);
-      }
-    `;
+  const  rectVerts = new Float32Array([
+    1.0, 1.0, 0.0, 1.0, 0.0,
+    1.0, -1.0, 0.0, 1.0, 1.0,
+    -1.0, -1.0, 0.0, 0.0, 1.0,
+    1.0, 1.0, 0.0, 1.0, 0.0,
+    -1.0, -1.0, 0.0, 0.0, 1.0,
+    -1.0, 1.0, 0.0, 0.0, 0.0,
+  ]);
+  
+  const verticesBuffer = device.createBuffer({
+    size: rectVerts.byteLength,
+    usage: GPUBufferUsage.VERTEX,
+    mappedAtCreation: true
+  });
+  new Float32Array(verticesBuffer.getMappedRange()).set(rectVerts);
+  verticesBuffer.unmap();
 
-    const vertexShaderWGSL = `
-      [[location 0]] var<in> pos : vec3<f32>;
-      [[location 1]] var<in> uv : vec2<f32>;
+  const bindGroupLayout = device.createBindGroupLayout({
+    entries: [{
+      // Sampler
+      binding: 0,
+      visibility: GPUShaderStage.FRAGMENT,
+      type: "sampler"
+    }, {
+      // Texture view
+      binding: 1,
+      visibility: GPUShaderStage.FRAGMENT,
+      type: "sampled-texture"
+    }]
+  });
 
-      [[builtin position]] var<out> Position : vec4<f32>;
-      
-      fn vtx_main() -> void {
-        Position = vec4<f32>(pos, 1.0);
-        return;
-      }
+  const pipeline = device.createRenderPipeline({
+    layout: device.createPipelineLayout({ bindGroupLayouts: [bindGroupLayout] }),
+    vertexStage: {
+      module: useWGSL
+        ? device.createShaderModule({
+            code: wgslShaders.vertex,
+          })
+        : device.createShaderModule({
+            code: glslShaders.vertex,
+            transform: (glsl) => glslang.compileGLSL(glsl, "vertex"),
+          }),
+      entryPoint: "main",
+    },
+    fragmentStage: {
+      module: useWGSL
+        ? device.createShaderModule({
+            code: wgslShaders.fragment,
+          })
+        : device.createShaderModule({
+            code: glslShaders.fragment,
+            transform: (glsl) => glslang.compileGLSL(glsl, "fragment"),
+          }),
+      entryPoint: "main",
+    },
 
-      entry_point vertex as "main" = vtx_main;
-    `;
+    primitiveTopology: "triangle-list",
 
-    const fragmentShaderGLSL = `#version 450
-      layout(location = 0) out vec4 outColor;
-
-      void main() {
-          outColor = vec4(1.0, 0.0, 0.0, 1.0);
-      }
-    `;
-
-    const fragmentShaderWGSL = `
-      [[location 0]] var<out> outColor : vec4<f32>;
-      fn frag_main() -> void {
-        outColor = vec4<f32>(1.0, 0.0, 0.0, 1.0);
-        return;
-      }
-      entry_point fragment as "main" = frag_main;
-    `;
-
-    const adapter = await navigator.gpu.requestAdapter();
-    const device = await adapter.requestDevice();
-    const glslang = await glslangModule();
-    const useWGSL = new URLSearchParams(window.location.search).get("wgsl") != "0";
-
-    const context = canvas.getContext('gpupresent');
-
-    const swapChainFormat = "bgra8unorm";
-
-    const  rectVerts = new Float32Array([
-      1.0, 1.0, 0.0, 1.0, 0.0,
-      1.0, -1.0, 0.0, 1.0, 1.0,
-      -1.0, -1.0, 0.0, 0.0, 1.0,
-      1.0, 1.0, 0.0, 1.0, 0.0,
-      -1.0, -1.0, 0.0, 0.0, 1.0,
-      -1.0, 1.0, 0.0, 0.0, 0.0,
-    ]);
-    
-    const verticesBuffer = device.createBuffer({
-      size: rectVerts.byteLength,
-      usage: GPUBufferUsage.VERTEX,
-      mappedAtCreation: true
-    });
-    new Float32Array(verticesBuffer.getMappedRange()).set(rectVerts);
-    verticesBuffer.unmap();
-
-    // @ts-ignore:
-    const swapChain: GPUSwapChain = context.configureSwapChain({
-      device,
-      format: swapChainFormat,
-    });
-
-    const pipeline = device.createRenderPipeline({
-      layout: device.createPipelineLayout({ bindGroupLayouts: [] }),
-
-      vertexStage: {
-        module: useWGSL ? createWGSLModule(device, vertexShaderWGSL)
-                        : createSPIRVModule(device, glslang, "vertex", vertexShaderGLSL),
-        entryPoint: "main"
-      },
-      fragmentStage: {
-        module: useWGSL ? createWGSLModule(device, fragmentShaderWGSL)
-                        : createSPIRVModule(device, glslang, "fragment", fragmentShaderGLSL),
-        entryPoint: "main"
-      },
-
-      primitiveTopology: "triangle-list",
-      vertexState: {
-        vertexBuffers: [{
-          arrayStride: 20,
-          attributes: [{
-            // position
-            shaderLocation: 0,
-            offset: 0,
-            format: "float3"
-          }, {
-            // uv
-            shaderLocation: 1,
-            offset: 12,
-            format: "float2"
-          }]
-        }],
-      },
-
-      colorStates: [{
-        format: swapChainFormat,
+    vertexState: {
+      vertexBuffers: [{
+        arrayStride: 20,
+        attributes: [{
+          // position
+          shaderLocation: 0,
+          offset: 0,
+          format: "float3"
+        }, {
+          // uv
+          shaderLocation: 1,
+          offset: 12,
+          format: "float2"
+        }]
       }],
-    });
+    },
 
-    function frame() {
-      const commandEncoder = device.createCommandEncoder({});
-      const textureView = swapChain.getCurrentTexture().createView();
+    colorStates: [
+      {
+        format: swapChainFormat,
+      },
+    ],
+  });
 
-      const renderPassDescriptor: GPURenderPassDescriptor = {
-        colorAttachments: [{
+  const rectTexture = await createTextureFromImage(device, 'assets/img/Di-3d.png', GPUTextureUsage.SAMPLED);
+
+  const sampler = device.createSampler({
+    magFilter: "linear",
+    minFilter: "linear",
+  });
+
+  const uniformBindGroup = device.createBindGroup({
+    layout: bindGroupLayout,
+    entries: [{
+      binding: 0,
+      resource: sampler,
+    }, {
+      binding: 1,
+      resource: rectTexture.createView(),
+    }],
+  });
+
+  function frame() {
+    const commandEncoder = device.createCommandEncoder();
+    const textureView = swapChain.getCurrentTexture().createView();
+
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+      colorAttachments: [
+        {
           attachment: textureView,
           loadValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-        }],
-      };
+        },
+      ],
+    };
 
-      const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-      passEncoder.setPipeline(pipeline);
-      passEncoder.setVertexBuffer(0, verticesBuffer);
-      passEncoder.draw(6, 1, 0, 0);
-      passEncoder.endPass();
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+    passEncoder.setPipeline(pipeline);
+    passEncoder.setVertexBuffer(0, verticesBuffer);
+    passEncoder.setBindGroup(0, uniformBindGroup);
+    passEncoder.draw(6, 1, 0, 0);
+    passEncoder.endPass();
 
-      device.defaultQueue.submit([commandEncoder.finish()]);
-    }
+    device.defaultQueue.submit([commandEncoder.finish()]);
+  }
 
-    return frame;
+  return frame;
 }
+
+export const glslShaders = {
+  vertex: `#version 450
+  layout(location = 0) in vec3 pos;
+  layout(location = 1) in vec2 uv;
+
+  layout(location = 0) out vec2 fragUV;
+  
+  void main() {
+    gl_Position = vec4(pos, 1.0);
+    fragUV = uv;
+  }
+`,
+
+  fragment: `#version 450
+  layout(set = 0, binding = 0) uniform sampler mySampler;
+  layout(set = 0, binding = 1) uniform texture2D myTexture;
+
+  layout(location = 0) in vec2 fragUV;
+  layout(location = 0) out vec4 outColor;
+
+  void main() {
+      outColor = texture(sampler2D(myTexture, mySampler), fragUV);
+  }
+`,
+};
+//outColor = texture_sample(myTexture, mySampler, fragUV);
+export const wgslShaders = {
+  vertex:`
+  [[location 0]] var<in> pos : vec3<f32>;
+  [[location 1]] var<in> uv : vec2<f32>;
+  [[location 0]] var<out> fragUV: vec2<f32>;
+
+  [[builtin position]] var<out> Position : vec4<f32>;
+  
+  fn vtx_main() -> void {
+    Position = vec4<f32>(pos, 1.0);
+    fragUV = uv;
+    return;
+  }
+
+  entry_point vertex as "main" = vtx_main;
+`,
+  fragment:  `
+  [[binding 0, set 0]] var<uniform> my_sampler: sampler;
+  [[binding 1, set 0]] var<uniform> my_texture: texture_sampled_2d<f32>;
+
+    [[location 0]] var<in> fragUV : vec2<f32>;
+    [[location 0]] var<out> outColor : vec4<f32>;
+    fn frag_main() -> void {
+      outColor = texture_sample(my_texture, my_sampler, fragUV);
+      return;
+    }
+    entry_point fragment as "main" = frag_main;
+  `,
+};
